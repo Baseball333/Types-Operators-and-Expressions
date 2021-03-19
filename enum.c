492a01e78167526c30917b309d1fd39f9b7e8a2a
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-03-19 13:25:00 -0700

    Escape sequences

--- enum.c
+++ enum.c
@@ -1,2 +1,5 @@
 /* Example of an enumeration constant */
 enum boolean{ NO, YES };
+
+/* The next enumeration represents the escape sequences in Unix */
+enum escapes { BELL = "\a", BACKSPACE = "\b", TAB = "\t", NEWLINE = "\n", VTAB = "\v", RETURN = "\r", };
