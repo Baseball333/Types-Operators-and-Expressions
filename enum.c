2ebf00e97d5141d2475c23ffe23466793e3daaea
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-03-19 13:28:33 -0700

    Write months

--- enum.c
+++ enum.c
@@ -3,3 +3,7 @@ enum boolean{ NO, YES };
 
 /* The next enumeration represents the escape sequences in Unix */
 enum escapes { BELL = "\a", BACKSPACE = "\b", TAB = "\t", NEWLINE = "\n", VTAB = "\v", RETURN = "\r", };
+
+enum months { JAN = 1, FEB, MAR, APR, MAY, JUN,
+              JUL, AUG, SEP, OCT, NOV, DEC };
+             /* Where the enumeration constant represents an unspecified progression */
